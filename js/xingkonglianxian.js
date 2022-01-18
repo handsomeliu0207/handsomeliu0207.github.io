08936f3f6bc22f699ea0bba9ee5387cc1329458f
xiaoliu0207 <15621432928>
2022-01-18 09:35:23 +0800

    Site updated: 2022-01-18 09:35:23

--- js/xingkonglianxian.js
+++ js/xingkonglianxian.js
@@ -152,6 +152,22 @@ function drawFrame() {
 
     window.requestAnimationFrame(drawFrame);
 }
-
+//判断设备
+function IsPC() {
+          var userAgentInfo = navigator.userAgent;
+          var Agents = ["Android", "iPhone",
+                      "SymbianOS", "Windows Phone",
+                      "iPad", "iPod"];
+          var flag = true;
+          for (var v = 0; v < Agents.length; v++) {
+              if (userAgentInfo.indexOf(Agents[v]) > 0) {
+                  flag = false;
+                 break;
+             }
+         }
+         return flag;
+      }
+if(IsPC()){
 initPoints(POINT_NUM);
 drawFrame();
+}
